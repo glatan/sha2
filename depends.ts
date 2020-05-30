5bccbe150cc0fd044180c62061a617a8fbb3973c
glatan <glatan.edu@gmail.com>
2020-05-30 16:39:02 +0900

    add 'concat' to depends.ts

--- depends.ts
+++ depends.ts
@@ -1,2 +1,3 @@
 export { assertEquals } from "https://deno.land/std@0.53.0/testing/asserts.ts";
+export { concat } from "https://deno.land/std@0.53.0/bytes/mod.ts";
 export { sprintf } from "https://deno.land/std@0.53.0/fmt/printf.ts";
